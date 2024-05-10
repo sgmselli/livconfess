85d06695d0de821bbf83f6f5c96c9079a0c4de90
Matthew Sellings <sellings.matthew@gmail.com>
2024-05-10 16:00:39 +0100

    change allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-usd&^ft#3n6*a*)^9%84iu62(r_irus+)77d(tvsyj904)1qew
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.56.97.96']
+ALLOWED_HOSTS = ['livconfessionsapi.top']
 
 
 # Application definition
