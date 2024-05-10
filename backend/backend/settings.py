f4645c92ce165a515805611d5c0570f86625c40d
Matthew Sellings <sellings.matthew@gmail.com>
2024-05-10 14:13:38 +0100

    allowed hosts changed

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-usd&^ft#3n6*a*)^9%84iu62(r_irus+)77d(tvsyj904)1qew
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.8.97.225']
 
 
 # Application definition
