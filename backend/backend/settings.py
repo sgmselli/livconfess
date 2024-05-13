9c7f5d49710dfc37112413df898a0e65a8676822
Matthew Sellings <sellings.matthew@gmail.com>
2024-05-13 19:40:10 +0100

    CHANGES

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,6 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
+import os
+
+# Retrieve the secret key from the environment variable
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
